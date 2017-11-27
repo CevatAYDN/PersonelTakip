98fe00943d19459b727a05d0eccb9c6b9b5201e0
CevatAYDN <cvtaydn53@gmail.com>
2017-11-27 18:06:40 +0300

    Repository Class lar eklendi

---
+++ Pt.Bl/AccountRepository/MemberShipTools.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Pt.Dal;
+using PT.Entity.IdentyModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pt.Bl.AccountRepository
+{
+    public class MemberShipTools
+    {
+        public static UserStore<ApplicationUser> NewUserStore() => new UserStore<ApplicationUser>(new Mycontext());
+        public static UserManager<ApplicationUser> NewUserManager() => new UserManager<ApplicationUser>(NewUserStore());
+        public static RoleStore<ApplicationRole> NewRoleStore() => new RoleStore<ApplicationRole>(new Mycontext());
+        public static RoleManager<ApplicationRole> NewRoleManager() => new RoleManager<ApplicationRole>(NewRoleStore());
+    }
+}
---
+++ Pt.Bl/Repository/RepositoryBase.cs
@@ -0,0 +1,84 @@
+﻿using Pt.Dal;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pt.Bl.Repository
+{
+    public class RepositoryBase<T, TId> where T : class, new()
+    {
+        protected internal static Mycontext dbContext;
+
+        public virtual List<T> GetAll()
+        {
+            try
+            {
+                dbContext = new Mycontext();
+                return dbContext.Set<T>().ToList();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+        public virtual T GetById(TId id)
+        {
+            try
+            {
+                dbContext = new Mycontext();
+
+                return dbContext.Set<T>().Find(id);
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+        public virtual int Insert(T entity)
+        {
+            try
+            {
+                dbContext = dbContext ?? new Mycontext();
+                dbContext.Set<T>().Add(entity);
+                return dbContext.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+        public virtual int Delete(T entity)
+        {
+            try
+            {
+                dbContext = dbContext ?? new Mycontext();
+                dbContext.Set<T>().Remove(entity);
+                return dbContext.SaveChanges();
+
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+        public  virtual int Update()
+        {
+            try
+            {
+                dbContext = dbContext ?? new Mycontext();
+                return dbContext.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+
+    }
+}
